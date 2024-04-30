4c54c41c4b07a1c823cef5a0875506aac2108fa6
Tech Deepak <141490582+techdeepak1996@users.noreply.github.com>
2024-04-30 15:55:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+document.getElementById('myForm').onsubmit = function(event) {
+      event.preventDefault();
+      const formData = new FormData(this);
+      let formInfo = '';
+      for (const pair of formData.entries()) {
+        formInfo += `${pair[0]}: ${pair[1]} `;
+      }
+      alert(formInfo.trim());
+    };
